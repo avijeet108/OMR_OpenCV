087ab3db9eb85e31b4b4541ac7ddca5d9da6e938
avijeet108 <kumaravijeet108@gamil.com>
2023-02-05 21:06:38 +0530

    Webcam Lag Fixed

--- OMR_Main.py
+++ OMR_Main.py
@@ -171,20 +171,17 @@ while True:
 
     stackedImage = utlis.stackImages(imageArray,0.5,lables)
     cv2.imshow("Result",stackedImage)
-    cv2.waitKey(5000)
-    break
-cv2.destroyAllWindows()
 
 
     # SAVE IMAGE WHEN 's' key is pressed
-#     if cv2.waitKey(1) & 0xFF == ord('s'):
-#         cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgFinal)
-#         cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
-#                       (1100, 350), (0, 255, 0), cv2.FILLED)
-#         cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
-#                     cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
-#         cv2.imshow('Result', stackedImage)
-#         cv2.waitKey(300)
-#         count += 1
-#         break
-# cv2.destroyAllWindows() 
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg",imgFinal)
+        cv2.rectangle(stackedImage, ((int(stackedImage.shape[1] / 2) - 230), int(stackedImage.shape[0] / 2) + 50),
+                      (1100, 350), (0, 255, 0), cv2.FILLED)
+        cv2.putText(stackedImage, "Scan Saved", (int(stackedImage.shape[1] / 2) - 200, int(stackedImage.shape[0] / 2)),
+                    cv2.FONT_HERSHEY_DUPLEX, 3, (0, 0, 255), 5, cv2.LINE_AA)
+        cv2.imshow('Result', stackedImage)
+        cv2.waitKey(300)
+        count += 1
+        break
+cv2.destroyAllWindows() 
